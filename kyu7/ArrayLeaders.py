7d5167aa9aa59a8f6b1ee90e47fd0bb419f36faf
vmred <vladdavidchenko@gmail.com>
2019-07-31 23:02:41 +0300

    Array leaders

---
+++ kyu7/ArrayLeaders.py
@@ -0,0 +1,25 @@
+# Introduction and Warm-up (Highly recommended)
+# Playing With Lists/Arrays Series
+# Definition
+# An element is leader if it is greater than The Sum all the elements to its right side.
+
+# Task
+# Given an array/list [] of integers , Find all the LEADERS in the array.
+
+# Notes
+# Array/list size is at least 3 .
+# Array/list's numbers Will be mixture of positives , negatives and zeros
+# Repetition of numbers in the array/list could occur.
+# Returned Array/list should store the leading numbers in the same order in the original array/list .
+
+# Input >> Output Examples
+# arrayLeaders ({1, 2, 3, 4, 0}) ==> return {4}
+from asserts.Asserts import assert_true
+
+
+def array_leaders(numbers):
+    return [numbers[x] for x in range(0, len(numbers)) if numbers[x] > sum(numbers[x + 1:])]
+
+
+assert_true(array_leaders([1, 2, 3, 4, 0]), [4])
+assert_true(array_leaders([16, 17, 4, 3, 5, 2]), [17, 5, 2])
