9d663de19a570d5a7a128e668912d2f577178896
vmred <vladdavidchenko@gmail.com>
2019-08-09 17:03:02 +0300

    Jumping number (Special number series #4)

---
+++ kyu7/JumpingNumber.py
@@ -0,0 +1,28 @@
+# Definition
+# Jumping number is the number that All adjacent digits in it differ by 1.
+
+# Task
+# Given a number, Find if it is Jumping or not .
+
+# Warm-up (Highly recommended)
+# Playing With Numbers Series
+# Notes
+# Number passed is always Positive .
+
+# Return the result as String .
+# The difference between ‘9’ and ‘0’ is not considered as 1 .
+# All single digit numbers are considered as Jumping numbers.
+
+# Input >> Output Examples
+# jumpingNumber(9) ==> return "Jumping!!"
+from asserts.Asserts import assert_true
+
+
+def jumping_number(number):
+    number = list(map(int, str(number)))
+    return ['Jumping!!', 'Not!!'][any(abs(number[x - 1] - number[x]) != 1 for x in range(1, len(number)))]
+
+
+assert_true(jumping_number(79), "Not!!")
+assert_true(jumping_number(98), "Jumping!!")
+assert_true(jumping_number(98789876), 'Jumping!!')
