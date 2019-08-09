d1726d54f55c5135f91a282e57cac47fd2d66361
vmred <vladdavidchenko@gmail.com>
2019-08-09 13:46:56 +0300

    String repeat

---
+++ kyu8/StringRepeat.py
@@ -0,0 +1,8 @@
+# Write a function called repeatString which repeats the given String src exactly count times.
+
+# repeatStr(6, "I") // "IIIIII"
+# repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+
+def repeat_str(repeat, string):
+    return repeat * string
