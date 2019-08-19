e6c42bb120b54d84c977e5730e52a33afeb77583
vmred <vladdavidchenko@gmail.com>
2019-08-19 18:48:03 +0300

    Replace with alphabet position

---
+++ kyu6/ReplaceWithAlphabetPosition.py
@@ -0,0 +1,18 @@
+# Welcome.
+
+# In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+# If anything in the text isn't a letter, ignore it and don't return it.
+# "a" = 1, "b" = 2, etc.
+
+# Example
+# alphabet_position("The sunset sets at twelve o' clock.")
+# Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+from asserts.Asserts import assert_true
+
+
+def alphabet_position(text):
+    return ' '.join([str(ord(x) - 96) for x in text.lower() if ord(x) - 96 > 0])
+
+
+assert_true(alphabet_position("The narwhal bacons at midnight."),
+            "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20")
