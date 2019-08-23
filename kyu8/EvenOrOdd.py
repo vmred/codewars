37beee348753cbde34d7b606a3e9997b8ae1191c
vmred <vladdavidchenko@gmail.com>
2019-08-23 23:13:52 +0300

    Even or Odd

---
+++ kyu8/EvenOrOdd.py
@@ -0,0 +1,10 @@
+# Create a function (or write a script in Shell) that takes an integer as an argument
+# and returns "Even" for even numbers or "Odd" for odd numbers.
+from asserts.Asserts import assert_true
+
+
+def even_or_odd(number):
+    return ['Even', 'Odd'][number % 2]
+
+
+assert_true(even_or_odd(2), "Even")
