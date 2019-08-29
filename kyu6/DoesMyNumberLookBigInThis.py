dd868c55e9558f2092ce726a102db5266765c1af
vmred <vladdavidchenko@gmail.com>
2019-08-29 13:21:40 +0300

    Does my number look big in this?

---
+++ kyu6/DoesMyNumberLookBigInThis.py
@@ -0,0 +1,27 @@
+# A Narcissistic Number is a number which is the sum of its own digits,
+# each raised to the power of the number of digits in a given base.
+# In this Kata, we will restrict ourselves to decimal (base 10).
+
+# For example, take 153 (3 digits):
+# 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+
+# and 1634 (4 digits):
+# 1^4 + 6^4 + 3^4 + 4^4 = 1 + 1296 + 81 + 256 = 1634
+
+
+# The Challenge:
+
+# Your code must return true or false depending upon whether the given number is a Narcissistic number in base 10.
+# Error checking for text strings or other invalid inputs is not required,
+# only valid integers will be passed into the function.
+from asserts.Asserts import assert_true
+
+
+def narcissistic(value):
+    power = len(str(value))
+    numbers = list(map(int, str(value)))
+
+    return value == sum([i ** power for i in numbers])
+
+
+assert_true(narcissistic(371), True)
