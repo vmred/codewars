4bca42e0e44273a59c3d51cc39374886b7c48cec
vmred <vladdavidchenko@gmail.com>
2019-08-30 23:16:26 +0300

    Name your python

---
+++ kyu8/NameYourPython.py
@@ -0,0 +1,19 @@
+# Python is now supported on Codewars!
+
+# For those of us who are not very familiar with Python,
+# let's handle the very basic challenge of creating a class named Python.
+#  We want to give our Pythons a name, so it should take a name argument that we can retrieve later.
+
+# For example:
+# bubba = Python('Bubba')
+# bubba.name # should return 'Bubba'
+from asserts.Asserts import assert_true
+
+
+class Python:
+    def __init__(self, name):
+        self.name = name
+
+
+bubba = Python('Bubba')
+assert_true(bubba.name, 'Bubba')
