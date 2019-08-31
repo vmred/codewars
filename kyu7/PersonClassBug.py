dd8169c56bb8029524c94316cbce002938b7304d
vmred <vladdavidchenko@gmail.com>
2019-08-31 16:16:05 +0300

    Person class bug

--- kyu7/PersonClassBug.py
+++ kyu7/PersonClassBug.py
@@ -15,9 +15,9 @@ from asserts.Asserts import assert_true
 
 class Person:
     def __init__(self, first_name, last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.full_name = '{} {}'.format(self.first_name, self.last_name)
+        # self.first_name = first_name
+        # self.last_name = last_name
+        self.full_name = '{} {}'.format(first_name, last_name)
         self.age = age
 
 
