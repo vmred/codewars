4ef0693b2796ebcff01540efbaa28539da63a861
vmred <vladdavidchenko@gmail.com>
2019-09-01 15:36:52 +0300

    Playing with cubes II

---
+++ kyu8/PlayingWithCubesII.py
@@ -0,0 +1,30 @@
+# Hey Codewarrior!
+
+# You already implemented a Cube class, but now we need your help again!
+# I'm talking about constructors. We don't have one.
+# Let's code two: One taking an integer and one handling no given arguments!
+
+# Also we got a problem with negative values. Correct the code so negative values will be switched to positive ones!
+# The constructor taking no arguments should assign 0 to Cube's Side property.
+from asserts.Asserts import assert_true
+
+
+class Cube(object):
+    # This cube needs help
+    # Define a constructor which takes one integer, or handles no args
+    def __init__(self, value=0):
+        self._side = value
+
+    def get_side(self):
+        """Return the side of the Cube"""
+        return self._side
+
+    def set_side(self, new_side):
+        """Set the value of the Cube's side."""
+        self._side = abs(new_side)
+
+
+c = Cube(10)
+assert_true(c.get_side(), 10)
+c = Cube()
+assert_true(c.get_side(), 0)
