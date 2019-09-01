93eb03b91b59505e5a911cfaeffb77cac613613b
vmred <vladdavidchenko@gmail.com>
2019-09-01 15:26:12 +0300

    Classy classes

---
+++ kyu8/ClassyClasses.py
@@ -0,0 +1,17 @@
+# Classy Classes
+# Basic Classes, this kata is mainly aimed at the new JS ES6 Update introducing classes
+# Task
+# Your task is to complete this Class, the Person class has been created. 
+# You must fill in the Constructor method to accept a name as string and an age as number, 
+# complete the get Info property and getInfo method/Info getter which should return
+# johns age is 34
+from asserts.Asserts import assert_true
+
+
+class Person:
+    def __init__(self, name, age):
+        self.info = f'{name}s age is {age}'
+
+
+person = Person('Alex', 16)
+assert_true(person.info, 'Alexs age is 16')
