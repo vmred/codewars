cce5b1cd0341a6424595275e4f6c1c448b552943
vmred <vladdavidchenko@gmail.com>
2019-09-03 16:11:02 +0300

    Python's dynamic classes #1

---
+++ kyu7/Python's Dynamic Classes #1.py
@@ -0,0 +1,10 @@
+def class_name_changer(cls, new_name):
+    if not new_name.isalnum() or not new_name[0].isupper():
+        raise Exception
+
+    cls.__name__ = new_name
+
+
+class MyClass(object):
+    def __str__(self):
+        return str(type(self))
