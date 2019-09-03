b7dc52e6ea0ac5c7b13371e78b9ad69a92b16614
vmred <vladdavidchenko@gmail.com>
2019-09-03 15:48:06 +0300

    Method for counting total occurence of specific digits

---
+++ kyu7/Method For Counting Total Occurence Of Specific Digits.py
@@ -0,0 +1,27 @@
+# We need a method in the List Class that may count specific digits from a given list of integers.
+# This marked digits will be given in a second list.
+# The method .count_spec_digits()/.countSpecDigits() will accept two arguments,
+# a list of an uncertain amount of integers integers_lists/integersLists (and of an uncertain amount of digits, too)
+# and a second list, digits_list/digitsList that has the specific digits to count which length cannot be longer than 10
+# (It's obvious, we've got ten digits).
+# The method will output a list of tuples, each tuple having two elements, the first one will be a digit to count,
+# and second one, its corresponding total frequency in all the integers of the first list.
+# This list of tuples should be ordered with the same order that the digits have in digitsList
+from asserts.Asserts import assert_true
+
+
+class List(object):
+    def count_spec_digits(self, integers_list, digits_list):
+        integers_list = ''.join(str(i) for i in integers_list)
+        return [(i, integers_list.count(str(i))) for i in digits_list]
+
+
+l = List()
+
+integers_list = [1, 1, 2, 3, 1, 2, 3, 4]
+digits_list = [1, 3]
+assert_true(l.count_spec_digits(integers_list, digits_list), [(1, 3), (3, 2)])
+
+integers_list = [-18, -31, 81, -19, 111, -888]
+digits_list = [1, 8, 4]
+assert_true(l.count_spec_digits(integers_list, digits_list), [(1, 7), (8, 5), (4, 0)])
