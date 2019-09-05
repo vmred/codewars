015a22169b39836605e59cfcf488feee7943338c
vmred <vladdavidchenko@gmail.com>
2019-09-05 18:30:09 +0300

    Sum of positive

---
+++ kyu8/Sum of positive.py
@@ -0,0 +1,11 @@
+# ou get an array of numbers, return the sum of all of the positives ones.
+# Example [1,-4,7,12] => 1 + 7 + 12 = 20
+# Note: if there is nothing to sum, the sum is default to 0.
+from asserts.Asserts import assert_true
+
+
+def positive_sum(arr):
+    return sum(x for x in arr if x > 0)
+
+
+assert_true(positive_sum([-1, 2, 3, 4, -5]), 9)
