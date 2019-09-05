7052dbc8bf7d3c2eaf35a034bf50cb9076fe9d34
vmred <vladdavidchenko@gmail.com>
2019-09-05 18:13:16 +0300

    Not prime numbers (not completed)

--- kyu6/NotPrimeNumbers.py
+++ kyu6/NotPrimeNumbers.py
@@ -9,23 +9,20 @@ from asserts.Asserts import assert_true
 
 
 def not_primes(a, b):
-    def is_prime(p):
-        if p == 2: return True
-        if p % 2 == 0: return False
-        max = p ** 0.5 + 1
-        i = 3
-        while i <= max:
-            if p % i == 0:
-                return False
-            i += 2
-        return True
+    def is_prime(n):
+        if not n % 2 and n > 2:
+            return False
 
-    # def is_prime(num):
-    #     return num > 1 and not any(num % n == 0 for n in range(2, num))
+        return all(n % i for i in range(3, int(n ** 0.5) + 1, 2))
 
-    return [x for x in range(a, b) if not is_prime(x) and all(i in {'2', '3', '5', '7'} for i in str(x))]
+    arr = [x for x in range(a, b) if all(i in {'2', '3', '5', '7'} for i in str(x))]
+    return [x for x in arr if not is_prime(x)]
+
+    # primes = [x for x in range(a, b) if not is_prime(x)]
+    # return [x for x in primes if all(i in str(x) for i in s)]
 
 
 assert_true(not_primes(2, 222), [22, 25, 27, 32, 33, 35, 52, 55, 57, 72, 75, 77])
-# assert_true(not_primes(2700, 3000),
-#             [2722, 2723, 2725, 2727, 2732, 2733, 2735, 2737, 2752, 2755, 2757, 2772, 2773, 2775])
+assert_true(not_primes(2, 77), [22, 25, 27, 32, 33, 35, 52, 55, 57, 72, 75])
+assert_true(not_primes(2700, 3000),
+            [2722, 2723, 2725, 2727, 2732, 2733, 2735, 2737, 2752, 2755, 2757, 2772, 2773, 2775])
