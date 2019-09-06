be76f88685d615e94a48014af4e71c63a9745db3
vmred <vladdavidchenko@gmail.com>
2019-09-06 13:40:53 +0300

    Remove first and last character

---
+++ kyu8/Remove First and Last Character.py
@@ -0,0 +1,13 @@
+# It's pretty straightforward.
+# Your goal is to create a function that removes the first and last characters of a string.
+# You're given one parameter, the original string.
+# You don't have to worry with strings with less than two characters.
+from asserts.Asserts import assert_true
+
+
+def remove_char(s):
+    return s[1:-1]
+
+
+assert_true(remove_char('eloquent'), 'loquen')
+assert_true(remove_char('ok'), '')
