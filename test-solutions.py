a4001d662527cf4567bca29ef7ff71e77abd5722
vmred <vladdavidchenko@gmail.com>
2019-09-10 16:59:59 +0300

    try to define suite via global params

--- test-solutions.py
+++ test-solutions.py
@@ -1,15 +1,29 @@
 import os
 import importlib
 
+import unittest
+
+suite = unittest.TestSuite()
+
 
 def collect_and_run(base_subdir):
     for root, subdirs, filenames in os.walk(base_subdir):
         for subdir in subdirs:
-            if not subdir.startswith('__'):
-                print('-->Info: root directory:', root, ', subdir:', subdir)
+            if not subdir.startswith('__') and subdir != 'tests':
                 submodule = importlib.import_module('.'.join((root, subdir, 'solution')))
-                submodule.test()
+
+                print('\n ---> INFO:', submodule.TestSolution)
+
+                # suite.addTest(submodule.TestSolution)
+                imported = importlib.__import__(str(submodule.TestSolution))
+                print('--> imported:', imported)
+                suite.addTest(importlib.import_module(imported))
+                print('--> suite:', suite)
 
 
 if int(os.getenv('beta', '1')) == 1:
     collect_and_run('beta')
+
+
+def run():
+    unittest.TextTestRunner(verbosity=2).run(suite)
