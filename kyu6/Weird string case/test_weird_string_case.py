9104af6555f4ddcbac0af098fba5f96edf491cc0
vmred <vladdavidchenko@gmail.com>
2019-09-15 18:43:54 +0300

    Weird string case

---
+++ kyu6/Weird string case/test_weird_string_case.py
@@ -0,0 +1,12 @@
+import importlib
+
+from asserts.Asserts import assert_true
+
+to_weird_case = importlib.import_module('kyu6.Weird string case.solution').to_weird_case
+
+
+class TestSolution:
+    def test_weird_string_case(self):
+        assert_true(to_weird_case('This'), 'ThIs')
+        assert_true(to_weird_case('is'), 'Is')
+        assert_true(to_weird_case('This is a test'), 'ThIs Is A TeSt')
