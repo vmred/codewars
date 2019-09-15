ddd0b30d5855d6e14e2a3143beadeea27a6267dc
vmred <vladdavidchenko@gmail.com>
2019-09-15 18:10:55 +0300

    Detect pangram

---
+++ kyu6/Detect pangram/test_detect_pangram.py
@@ -0,0 +1,10 @@
+from asserts.Asserts import assert_true
+import importlib
+
+is_pangram = importlib.import_module('kyu6.Detect pangram.solution').is_pangram
+
+
+class TestSolution:
+    def test_pangram(self):
+        assert_true(is_pangram("The quick, brown fox jumps over the lazy dog!"), True)
+        assert_true(is_pangram("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"), False)
