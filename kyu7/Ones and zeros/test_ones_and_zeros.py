c51e86bc157232c51cb9017c2784426e76d9e2ae
vmred <vladdavidchenko@gmail.com>
2019-09-15 18:52:48 +0300

    Ones and zeros

---
+++ kyu7/Ones and zeros/test_ones_and_zeros.py
@@ -0,0 +1,13 @@
+import importlib
+
+from asserts.Asserts import assert_true
+
+binary_array_to_number = importlib.import_module('kyu7.Ones and zeros.solution').binary_array_to_number
+
+
+class TestSolution:
+    def test_binary_array_to_number(self):
+        assert_true(binary_array_to_number([0, 0, 0, 1]), 1)
+        assert_true(binary_array_to_number([0, 0, 1, 0]), 2)
+        assert_true(binary_array_to_number([1, 1, 1, 1]), 15)
+        assert_true(binary_array_to_number([0, 1, 1, 0]), 6)
