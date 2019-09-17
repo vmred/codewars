5e69a2ea49b9ba32254fc1ede6647880a0fc82d2
vmred <vladdavidchenko@gmail.com>
2019-09-17 12:23:58 +0300

    Sum of the first nth term of series

---
+++ kyu7/Sum of the first nth term of Series/test_sum_of_the_first_nth_term_of_series.py
@@ -0,0 +1,12 @@
+import importlib
+
+from asserts.Asserts import assert_true
+
+series_sum = importlib.import_module('kyu7.Sum of the first nth term of Series.solution').series_sum
+
+
+class TestSolution:
+    def test_sum_of_the_first_nth_term_of_series(self):
+        assert_true(series_sum(1), "1.00")
+        assert_true(series_sum(2), "1.25")
+        assert_true(series_sum(3), "1.39")
