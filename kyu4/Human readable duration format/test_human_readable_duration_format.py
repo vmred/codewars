fca8d91506ea56437c9b578ba51ac954efb68010
vmred <vladdavidchenko@gmail.com>
2019-09-20 13:19:22 +0300

    Human readable duration format

---
+++ kyu4/Human readable duration format/test_human_readable_duration_format.py
@@ -0,0 +1,14 @@
+import importlib
+
+from asserts.Asserts import assert_true
+
+format_duration = importlib.import_module('kyu4.Human readable duration format.solution').format_duration
+
+
+class TestSolution:
+    def test_human_readable_duration_format(self):
+        assert_true(format_duration(1), "1 second")
+        assert_true(format_duration(62), "1 minute and 2 seconds")
+        assert_true(format_duration(120), "2 minutes")
+        assert_true(format_duration(3600), "1 hour")
+        assert_true(format_duration(3662), "1 hour, 1 minute and 2 seconds")
