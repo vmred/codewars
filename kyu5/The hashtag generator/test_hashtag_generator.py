6c689fd8137b41b1e5aea6c4abae136f640c7796
vmred <vladdavidchenko@gmail.com>
2019-09-26 14:23:47 +0300

    The hashtag generator

---
+++ kyu5/The hashtag generator/test_hashtag_generator.py
@@ -0,0 +1,22 @@
+import importlib
+
+from asserts.Asserts import assert_true
+
+generate_hashtag = importlib.import_module('kyu5.The hashtag generator.solution').generate_hashtag
+
+
+class TestSolution:
+    def test_hashtag_generator(self):
+        assert_true(generate_hashtag(''), False)
+        assert_true(generate_hashtag('Do We have A Hashtag')[0], '#')
+        assert_true(generate_hashtag('Codewars'), '#Codewars')
+        assert_true(generate_hashtag('Codewars      '), '#Codewars')
+        assert_true(generate_hashtag('Codewars Is Nice'), '#CodewarsIsNice')
+        assert_true(generate_hashtag('codewars is nice'), '#CodewarsIsNice')
+        assert_true(generate_hashtag('CodeWars is nice'), '#CodewarsIsNice')
+        assert_true(generate_hashtag('c i n'), '#CIN')
+        assert_true(generate_hashtag('codewars  is  nice'), '#CodewarsIsNice')
+        assert_true(generate_hashtag(
+            'Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo'
+            'oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat'),
+            False)
