caba4111460dcd5b883c9390a5e127bda403542c
vmred <vladdavidchenko@gmail.com>
2019-10-05 23:59:40 +0300

    Alphabetical addition

---
+++ kyu7/Alphabetical addition/test_alphabethical_addition.py
@@ -0,0 +1,12 @@
+import importlib
+
+from asserts.Asserts import assert_true
+
+add_letters = importlib.import_module('kyu7.Alphabetical addition.solution').add_letters
+
+
+class TestSolution:
+    def test_alphabet_addition(self):
+        # assert_true(add_letters('a', 'b', 'c'), 'f')
+        # assert_true(add_letters('z', 'a'), 'a')
+        assert_true(add_letters("u", "n", "e", "v"), 'a')
