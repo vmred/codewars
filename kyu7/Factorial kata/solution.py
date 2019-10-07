ded6849f6fc9b420339aa9bc9e49ea88e1fe670b
vmred <vladdavidchenko@gmail.com>
2019-10-07 17:35:06 +0300

    Factorial

---
+++ kyu7/Factorial kata/solution.py
@@ -0,0 +1,20 @@
+# In mathematics, the factorial of a non-negative integer n, denoted by n!,
+# is the product of all positive integers less than or equal to n.
+# For example: 5! = 5 * 4 * 3 * 2 * 1 = 120.
+# By convention the value of 0! is 1.
+
+# Write a function to calculate factorial for a given input.
+# If input is below 0 or above 12 throw an exception of type ArgumentOutOfRangeException (C#)
+# or IllegalArgumentException (Java)
+# or RangeException (PHP)
+# or throw a RangeError (JavaScript)
+# or ValueError (Python).
+
+
+import math
+
+
+def factorial(n):
+    if n < 0 or n > 12:
+        raise ValueError()
+    return math.factorial(n)
