c44a3d40f1ec81e5c681b3df1faf1bf0f6324c7c
vmred <vladdavidchenko@gmail.com>
2019-10-11 16:52:34 +0300

    Basic math (add or subtract)

---
+++ kyu7/Basic Math (Add or Subtract)/solution.py
@@ -0,0 +1,12 @@
+# In this kata, you will do addition and subtraction on a given string.
+# The return value must be also a string.
+
+# Note: the input will not be empty.
+
+# Examples
+# "1plus2plus3plus4"  --> "10"
+# "1plus2plus3minus4" -->  "2"
+
+
+def calculate(s):
+    return str(eval(s.replace('plus', '+').replace('minus', '-')))
