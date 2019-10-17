d4d6f16329eaba8c3a17be1f674a68bd0531b8d9
vmred <vladdavidchenko@gmail.com>
2019-10-17 17:32:35 +0300

    Bingo ( Or Not )

---
+++ kyu7/Bingo ( Or Not )/solution.py
@@ -0,0 +1,11 @@
+# For this game of BINGO, you will receive a single array of 10 numbers from 1 to 26 as an input.
+# Duplicate numbers within the array are possible.
+
+# Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc).
+# Write a function where you will win the game if your numbers can spell "BINGO".
+# They do not need to be in the right order in the input array). Otherwise you will lose.
+# Your outputs should be "WIN" or "LOSE" respectively.
+
+
+def bingo(array):
+    return ['LOSE', 'WIN'][all(x in [chr(i + 96) for i in array] for x in ['b', 'i', 'n', 'g', 'o'])]
