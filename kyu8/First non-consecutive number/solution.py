6996db50db39bcd05707e159265d0d7691dad0ae
vmred <vladdavidchenko@gmail.com>
2019-10-17 15:35:41 +0300

    First non-consecutive number

--- kyu8/First non-consecutive number/solution.py
+++ kyu8/First non-consecutive number/solution.py
@@ -16,9 +16,6 @@
 
 
 def first_non_consecutive(arr):
-    non_consecutive = None
     for i in range(1, len(arr)):
         if arr[i] - arr[i - 1] > 1:
             return arr[i]
-
-    return non_consecutive
