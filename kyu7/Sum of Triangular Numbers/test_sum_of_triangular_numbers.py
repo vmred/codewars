a6295a89db71e7f75dc557fccc185ab1ced4090e
vmred <vladdavidchenko@gmail.com>
2019-10-19 21:33:05 +0300

    Sum of triangular numbers

---
+++ kyu7/Sum of Triangular Numbers/test_sum_of_triangular_numbers.py
@@ -0,0 +1,13 @@
+from asserts.Asserts import assert_true
+import importlib
+
+sum_triangular_numbers = importlib.import_module('kyu7.Sum of Triangular Numbers.solution').sum_triangular_numbers
+
+
+class TestSolution:
+    def test_sum_of_triangular_numbers(self):
+        assert_true(sum_triangular_numbers(6), 56)
+        assert_true(sum_triangular_numbers(34), 7140)
+        assert_true(sum_triangular_numbers(-291), 0)
+        assert_true(sum_triangular_numbers(943), 140205240)
+        assert_true(sum_triangular_numbers(-971), 0)
