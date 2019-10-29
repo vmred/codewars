4742bd4fda1d697865e087c1b226ae579111e9db
vmred <vladdavidchenko@gmail.com>
2019-10-29 22:30:11 +0200

    Keep Hydrated!

---
+++ kyu8/Keep Hydrated!/test_keep_hydrated.py
@@ -0,0 +1,15 @@
+from asserts.Asserts import assert_true
+import importlib
+
+litres = importlib.import_module('kyu8.Keep Hydrated!.solution').litres
+
+
+class TestSolution:
+    def test_keep_hydrated(self):
+        assert_true(litres(2), 1)
+        assert_true(litres(1.4), 0)
+        assert_true(litres(12.3), 6)
+        assert_true(litres(0.82), 0)
+        assert_true(litres(11.8), 5)
+        assert_true(litres(1787), 893)
+        assert_true(litres(0), 0)
