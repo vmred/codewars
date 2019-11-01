cce9c77012423961825e26efaa2c4765a08a7250
vmred <vladdavidchenko@gmail.com>
2019-11-01 12:42:38 +0200

    Reverse words

---
+++ kyu7/Reverse words/solution.py
@@ -0,0 +1,9 @@
+# Complete the function that accepts a string parameter, and reverses each word in the string.
+# All spaces in the string should be retained.
+
+# Examples
+# "This is an example!" ==> "sihT si na !elpmaxe"
+# "double  spaces"      ==> "elbuod  secaps"
+
+def reverse_words(text):
+    return ' '.join([i[::-1] for i in text.split(' ')])
---
+++ kyu7/Reverse words/test_reverse_words.py
@@ -0,0 +1,13 @@
+from asserts.Asserts import assert_true
+import importlib
+
+reverse_words = importlib.import_module('kyu7.Reverse words.solution').reverse_words
+
+
+class TestSolution:
+    def test_reverse_words(self):
+        assert_true(reverse_words('The quick brown fox jumps over the lazy dog.'),
+                           'ehT kciuq nworb xof spmuj revo eht yzal .god')
+        assert_true(reverse_words('apple'), 'elppa')
+        assert_true(reverse_words('a b c d'), 'a b c d')
+        assert_true(reverse_words('double  spaced  words'), 'elbuod  decaps  sdrow')
