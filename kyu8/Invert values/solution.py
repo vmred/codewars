099010d5ee0ab0703e49ca3b049406c0cc2c1294
vmred <vladdavidchenko@gmail.com>
2019-11-05 22:04:33 +0200

    Invert values

---
+++ kyu8/Invert values/solution.py
@@ -0,0 +1,10 @@
+# Given a set of numbers, return the additive inverse of each.
+# Each positive becomes negatives, and the negatives become positives.
+
+# invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+# invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+# invert([]) == []
+
+
+def invert(lst):
+    return [-i for i in lst]
---
+++ kyu8/Invert values/test_invert_values.py
@@ -0,0 +1,11 @@
+from asserts.Asserts import assert_true
+import importlib
+
+invert = importlib.import_module('kyu8.Invert values.solution').invert
+
+
+class TestSolution:
+    def test_invert_values(self):
+        assert_true(invert([1, 2, 3, 4, 5]), [-1, -2, -3, -4, -5])
+        assert_true(invert([1, -2, 3, -4, 5]), [-1, 2, -3, 4, -5])
+        assert_true(invert([]), [])
