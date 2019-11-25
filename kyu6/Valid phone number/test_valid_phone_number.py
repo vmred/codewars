24582b2fda1c861353cdf2130ebbc0d808e89987
vmred <vladdavidchenko@gmail.com>
2019-11-25 11:43:45 +0200

    Valid phone number

---
+++ kyu6/Valid phone number/test_valid_phone_number.py
@@ -0,0 +1,9 @@
+from asserts.Asserts import assert_true
+import importlib
+
+validPhoneNumber = importlib.import_module('kyu6.Valid phone number.solution').validPhoneNumber
+
+
+class TestSolution:
+    def test_valid_phone_number(self):
+        assert_true(validPhoneNumber("(123) 456-7890"), True)
