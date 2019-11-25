f3b45be552ddc9e0da8b9428a1d07b5fccb793ba
vmred <vladdavidchenko@gmail.com>
2019-11-25 12:47:14 +0200

    Validate credit card number

---
+++ kyu6/Validate credit card number/test_validate_credit_card_number.py
@@ -0,0 +1,12 @@
+from asserts.Asserts import assert_true
+import importlib
+
+validate = importlib.import_module('kyu6.Validate credit card number.solution').validate
+
+
+class TestSolution:
+    def test_validate_credit_card_number(self):
+        assert_true(validate(123), False)
+        assert_true(validate(1), False)
+        assert_true(validate(2121), True)
+        assert_true(validate(1230), True)
