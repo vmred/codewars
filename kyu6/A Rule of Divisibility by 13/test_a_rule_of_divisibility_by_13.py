2529f4673e33db44e7c305425416227b0030cc53
vmred <vladdavidchenko@gmail.com>
2019-12-11 14:45:59 +0200

    A rule of divisibilty by 13

---
+++ kyu6/A Rule of Divisibility by 13/test_a_rule_of_divisibility_by_13.py
@@ -0,0 +1,10 @@
+from asserts.Asserts import assert_true
+import importlib
+
+thirt = importlib.import_module('kyu6.A Rule of Divisibility by 13.solution').thirt
+
+
+class TestSolution:
+    def test_a_rule_of_divisibility_by_13(self):
+        assert_true(thirt(8529), 79)
+        assert_true(thirt(1234567), 87)
