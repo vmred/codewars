37a0844465e1e07a5ab460077db60a806506504f
vmred <vladdavidchenko@gmail.com>
2019-12-30 14:48:46 +0200

    getting started #let's piay

--- kyu6/Getting started #lets piay/solution.py
+++ kyu6/Getting started #lets piay/solution.py
@@ -7,19 +7,10 @@ from math import pi
 
 
 def whatpimeans(alpha='abcdefghijklmnopqrstuvwxyz'):
-    # create a dictionnary linking alphabet to 'secret encryption'
-    # dico = {85:'a', 24:'b',32:'c', [...],10:'z'}
     dico = {k: v for k, v in
             zip([85, 24, 32, 64, 11, 52, 91, 79, 78, 99, 62, 27, 74, 35, 14,
                  16, 66, 81, 19, 39, 13, 33, 45, 49, 95, 10], alpha.upper())}
 
-    # take the number PI as string and prepare it for decoding
     crypt = str(pi).replace('.', '')[::-1]
-    # reverse the string and group 2 by 2 to form a list
-    code = [int(crypt[i:i+2]) for i in range(0, len(crypt), 2)]
-
-    # take the modified string and try to decode
-
-    decrypt = ''.join([dico[x] for x in code])
-    # stuck somewhere ? try to print() some of the steps
-    return decrypt
+    code = [int(crypt[i:i + 2]) for i in range(0, len(crypt), 2)]
+    return ''.join([dico[x] for x in code])
