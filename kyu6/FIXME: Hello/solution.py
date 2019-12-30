60622d83f0464baccc9abc3b05a771f02ec888e0
vmred <vladdavidchenko@gmail.com>
2019-12-30 12:47:10 +0200

    FIXME: Hello

---
+++ kyu6/FIXME: Hello/solution.py
@@ -0,0 +1,50 @@
+# The code provided has a method hello which is supposed to show only those attributes which have been explicitly set.
+# Furthermore, it is supposed to say them in the same order they were set.
+
+# But it's not working properly.
+
+# Notes
+# There are 3 attributes
+
+# name
+# age
+# sex ('M' or 'F')
+# When the same attribute is assigned multiple times the hello method shows it only once.
+# If this happens the order depends on the first assignment of that attribute,
+# but the value is from the last assignment.
+
+# Examples
+# Hello.
+# Hello. My name is Bob. I am 27. I am male.
+# Hello. I am 27. I am male. My name is Bob.
+# Hello. My name is Alice. I am female.
+# Hello. My name is Batman.
+# Task
+# Fix the code so we can all go home early.
+
+
+class Dinglemouse(object):
+
+    def __init__(self):
+        self.name = None
+        self.sex = None
+        self.age = None
+        self.hello_message = {'base': 'Hello'}
+
+    def setAge(self, age):
+        self.age = age
+        self.hello_message['age'] = f'I am {self.age}'
+        return self
+
+    def setSex(self, sex):
+        self.sex = ['female', 'male'][sex.upper() == 'M']
+        self.hello_message['sex'] = f'I am {self.sex}'
+        return self
+
+    def setName(self, name):
+        self.name = name
+        self.hello_message['name'] = f'My name is {self.name}'
+        return self
+
+    def hello(self):
+        return '. '.join(list(self.hello_message.values())) + '.'
