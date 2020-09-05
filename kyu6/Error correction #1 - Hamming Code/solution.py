4d7f72ac05bd67335326aeff87eb7f873e36ee5e
vmred <vladdavidchenko@gmail.com>
2020-09-05 23:43:34 +0300

    refactor

--- kyu6/Error correction #1 - Hamming Code/solution.py
+++ kyu6/Error correction #1 - Hamming Code/solution.py
@@ -6,17 +6,5 @@ def encode(string):
     return ''.join([bin(ord(x))[2:].zfill(8) for x in string]).replace('0', '000').replace('1', '111')
 
 def decode(bits):
-    sp = wrap(bits, 3)
-    for i, v in enumerate(sp):
-        if v == '111':
-            sp[i] = '1'
-        elif v == '000':
-            sp[i] = '0'
-        elif v.count('0') > 1:
-            sp[i] = '0'
-        else:
-            sp[i] = '1'
-
-    sp = int(''.join(sp), 2)
-
-    return sp.to_bytes((sp.bit_length() + 7) // 8, 'big').decode()
+    bits = int(''.join(''.join(['1' if x.count('1') > x.count('0') else '0' for x in wrap(bits, 3)])), 2)
+    return bits.to_bytes((bits.bit_length() + 7) // 8, 'big').decode()
