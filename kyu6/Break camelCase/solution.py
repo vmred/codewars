af2b497f7305f9cff30daca2a4836e17cce5b611
vmred <vladdavidchenko@gmail.com>
2022-03-27 22:10:55 +0300

    kyu6: Break camelCase

---
+++ kyu6/Break camelCase/solution.py
@@ -0,0 +1,10 @@
+# Complete the solution so that the function will break up camel casing, using a space between words.
+
+# Example
+# "camelCasing"  =>  "camel Casing"
+# "identifier"   =>  "identifier"
+# ""             =>  ""
+
+def solution(value):
+    return ''.join(x if x.islower() else f" {x.upper()}"  for x in value)
+
