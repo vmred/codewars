5c1a0fb381af33decfe9d97811a2d2e15fce4552
vmred <vladdavidchenko@gmail.com>
2023-04-17 21:00:51 +0300

    add(): solution for kyu7: Exclamation marks series #7: Remove words from the sentence if it contains one exclamation mark

---
+++ kyu7/Remove words from the sentence if it contains one exclamation mark/solution.py
@@ -0,0 +1,14 @@
+# Remove words from the sentence if they contain exactly one exclamation mark.
+# Words are separated by a single space, without leading/trailing spaces.
+#
+# Examples
+# remove("Hi!") === ""
+# remove("Hi! Hi!") === ""
+# remove("Hi! Hi! Hi!") === ""
+# remove("Hi Hi! Hi!") === "Hi"
+# remove("Hi! !Hi Hi!") === ""
+# remove("Hi! Hi!! Hi!") === "Hi!!"
+# remove("Hi! !Hi! Hi!") === "!Hi!"
+
+def remove(s):
+    return ' '.join([x for x in s.split() if x.count('!') != 1])
