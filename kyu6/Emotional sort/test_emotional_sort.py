d25b10b28042dd71732223a5b7540c002e96ffd3
vmred <vladdavidchenko@gmail.com>
2023-04-21 00:59:12 +0300

    add(): solution for "kyu6: Emotional sort"

---
+++ kyu6/Emotional sort/test_emotional_sort.py
@@ -0,0 +1,22 @@
+import pytest
+from asserts.asserts import assert_true
+import importlib
+from utils.utils import TestCase
+
+solution = importlib.import_module('kyu6.Emotional sort.solution').sort_emotions
+
+cases = [
+    TestCase([[':D', 'T_T', ':D', ':('], True], [':D', ':D', ':(', 'T_T']),
+    TestCase([['T_T', ':D', ':(', ':('], True], [':D', ':(', ':(', 'T_T']),
+    TestCase([[':)', 'T_T', ':)', ':D', ':D'], True], [':D', ':D', ':)', ':)', 'T_T']),
+    TestCase([[':D', 'T_T', ':D', ':('], False], ['T_T', ':(', ':D', ':D']),
+    TestCase([[], False], []),
+    TestCase([[], True], []),
+]
+
+
+class TestSolution:
+
+    @pytest.mark.parametrize('test', cases, ids=[f'{test.test_data}' for test in cases])
+    def test_emotional_sort(self, test):
+        assert_true(solution(*test.test_data), test.test_output)
