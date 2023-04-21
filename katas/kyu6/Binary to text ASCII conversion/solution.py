b464525db0eac2082bd9509d83bc67a3ece6f02d
vmred <vladdavidchenko@gmail.com>
2023-04-21 12:34:51 +0300

    add(): solution for "kyu6: Binary to text (ASCII) conversion"

---
+++ katas/kyu6/Binary to text ASCII conversion/solution.py
@@ -0,0 +1,10 @@
+# Write a function that takes in a binary string and returns the equivalent decoded text (the text is ASCII encoded).
+# Each 8 bits on the binary string represent 1 character on the ASCII table.
+# The input string will always be a valid binary string.
+# Characters can be in the range from "00000000" to "11111111" (inclusive)
+# Note: In the case of an empty binary string your function should return an empty string.
+
+def binary_to_string(binary):
+    if not binary: return ''
+    n = int(binary, 2)
+    return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode()
---
+++ katas/kyu6/Binary to text ASCII conversion/test_binary_to_text_ascii_conversion.py
@@ -0,0 +1,18 @@
+import pytest
+from asserts.asserts import assert_true
+import importlib
+from asserts.testcase import TestCase
+
+solution = importlib.import_module('katas.kyu6.Binary to text ASCII conversion.solution').binary_to_string
+
+cases = [
+    TestCase('0100100001100101011011000110110001101111', 'Hello'),
+    TestCase('00110001001100000011000100110001', '1011'),
+]
+
+
+class TestSolution:
+
+    @pytest.mark.parametrize('test', cases, ids=[f'{test.test_data}' for test in cases])
+    def test_binary_to_text_ascii_conversion(self, test):
+        assert_true(solution(test.test_data), test.test_output)
