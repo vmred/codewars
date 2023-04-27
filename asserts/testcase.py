97be328061ec613f0ab487fc1a3a59ac6e6c6918
vmred <vladdavidchenko@gmail.com>
2023-04-27 21:21:01 +0300

    task(26724548): fix defaults for test_function

--- asserts/testcase.py
+++ asserts/testcase.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, Optional
 
 
 @dataclass
 class TestCase:
     test_data: Any
     test_output: Any
-    test_function: Any
+    test_function: Optional[Any] = None
