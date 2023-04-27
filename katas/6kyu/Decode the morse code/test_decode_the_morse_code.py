d041bf93929de50f4d764367bf051100fc9491ca
vmred <vladdavidchenko@gmail.com>
2023-04-27 22:28:51 +0300

    task(26724548): fix "Decode the morse code" kata

--- katas/6kyu/Decode the morse code/test_decode_the_morse_code.py
+++ katas/6kyu/Decode the morse code/test_decode_the_morse_code.py
@@ -3,13 +3,19 @@ import pytest
 from asserts.asserts import assert_true
 import importlib
 
-decodeMorse = importlib.import_module('katas.6kyu.Decode the morse code.solution').decodeMorse
+from asserts.testcase import TestCase
+
+solution = importlib.import_module('katas.6kyu.Decode the morse code.solution').decodeMorse
+
+cases = [
+    TestCase('.... . -.--   .--- ..- -.. .', 'HEY JUDE'),
+    TestCase('...---...', 'SOS'),
+    TestCase('.   .', 'E E'),
+    TestCase('----- .---- ..--- ---.. ----.', '01289')
+]
 
 
 class TestSolution:
-    @pytest.mark.not_completed
-    @pytest.mark.xfail(reason='not completed')
-    def test_decode_the_morse_code(self):
-        assert_true(decodeMorse('.... . -.--   .--- ..- -.. .'), 'HEY JUDE')
-        assert_true(decodeMorse('...---...'), 'SOS')
-        assert_true(decodeMorse('  E E '), 'E E')
+    @pytest.mark.parametrize('test', cases, ids=[f'{test.test_data}' for test in cases])
+    def test_decode_the_morse_code(self, test):
+        assert_true(solution(test.test_data), test.test_output)
