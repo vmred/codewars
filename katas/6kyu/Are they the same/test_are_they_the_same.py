7c6b7c8a996e38de209626f3fb1a291325a9c4d6
vmred <vladdavidchenko@gmail.com>
2023-05-04 21:24:36 +0300

    update

--- katas/6kyu/Are they the same/test_are_they_the_same.py
+++ katas/6kyu/Are they the same/test_are_they_the_same.py
@@ -1,11 +1,19 @@
-from asserts.asserts import assert_true
 import importlib
+import pytest
 
-comp = importlib.import_module('katas.6kyu.Are they the same.solution').comp
+from asserts.asserts import assert_true
+
+from asserts.testcase import TestCase
+
+solution = importlib.import_module('katas.6kyu.Are they the same.solution').comp
+
+cases = [
+    TestCase([[121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 361, 25921, 361, 20736, 361]], True),
+    TestCase([[10, 99], [101, 9801]], False),
+]
 
 
 class TestSolution:
-    def test_are_they_the_same(self):
-        assert_true(comp([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 361, 25921, 361, 20736, 361]), True)
-        assert_true(comp([10, 99], [101, 9801]), False)
-        assert_true(comp([], []), True)
+    @pytest.mark.parametrize('test', cases, ids=[f'{test.test_data}' for test in cases])
+    def test_explosive_sum(self, test):
+        assert_true(solution(*test.test_data), test.test_output)
