ad2bf5e077a3cee6c0980308ee8a9f2f03415db6
red-redalert <132378585+red-redalert@users.noreply.github.com>
2023-05-04 21:26:49 +0300

    task(26995282): fix pylint for 6kyu folder & use new test template (#47)
    Co-authored-by: vmred

--- katas/6kyu/A Rule of Divisibility by 13/test_a_rule_of_divisibility_by_13.py
+++ katas/6kyu/A Rule of Divisibility by 13/test_a_rule_of_divisibility_by_13.py
@@ -1,10 +1,19 @@
-from asserts.asserts import assert_true
 import importlib
+import pytest
+from asserts.asserts import assert_true
+from asserts.testcase import TestCase
+
 
 solution = importlib.import_module('katas.6kyu.A Rule of Divisibility by 13.solution').thirt
 
 
+cases = [
+    TestCase(8529, 79),
+    TestCase(1234567, 87),
+]
+
+
 class TestSolution:
-    def test_a_rule_of_divisibility_by_13(self):
-        assert_true(solution(8529), 79)
-        assert_true(solution(1234567), 87)
+    @pytest.mark.parametrize('test', cases, ids=[f'{test.test_data}' for test in cases])
+    def test_a_rule_of_divisibility_by_13(self, test):
+        assert_true(solution(test.test_data), test.test_output)
