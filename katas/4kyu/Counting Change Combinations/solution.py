e792e38b1c9fda00041bffa996ee85e2c93bc6f8
vmred <vladdavidchenko@gmail.com>
2023-05-04 19:17:48 +0300

    task(27368631): slightly change condition

--- katas/4kyu/Counting Change Combinations/solution.py
+++ katas/4kyu/Counting Change Combinations/solution.py
@@ -18,6 +18,6 @@
 def count_change(money, coins):
     if money == 0:
         return 1
-    elif money < 0 or len(coins) == 0:
+    if money < 0 or not coins:
         return 0
     return count_change(money - coins[0], coins) + count_change(money, coins[1:])
