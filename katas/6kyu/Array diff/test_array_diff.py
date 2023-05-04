f060065242cf8a96d37581fd55beb0e549f8738d
red-redalert <132378585+red-redalert@users.noreply.github.com>
2023-05-04 22:15:34 +0300

    update 6kyu tests

--- katas/6kyu/Array diff/test_array_diff.py
+++ katas/6kyu/Array diff/test_array_diff.py
@@ -1,10 +1,24 @@
-from asserts.asserts import assert_true
 import importlib
+import pytest
+from asserts.asserts import assert_true
+from asserts.test_case import TestCase
 
-array_diff = importlib.import_module('katas.6kyu.Array diff.solution').array_diff
+
+solution = importlib.import_module('katas.6kyu.Array diff.solution').array_diff
 
 
 class TestSolution:
     def test_array_diff(self):
         assert_true(array_diff([1, 2], [1]), [2])
         assert_true(array_diff([1, 2, 2], []), [1, 2, 2])
+
+cases = [
+    TestCase([[1, 2], [1]], [2]),
+    TestCase([[1, 2, 2], []], [1, 2, 2]),
+]
+
+
+class TestSolution:
+    @pytest.mark.parametrize('test', cases, ids=[f'{test.test_data}' for test in cases])
+    def test_array_diff(self, test):
+        assert_true(solution(*test.test_data), test.test_output)
