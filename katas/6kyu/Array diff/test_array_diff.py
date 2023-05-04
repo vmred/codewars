72216963414e09a4216c55b7a277d397336d043a
vmred <vladdavidchenko@gmail.com>
2023-05-04 22:18:26 +0300

    upd

--- katas/6kyu/Array diff/test_array_diff.py
+++ katas/6kyu/Array diff/test_array_diff.py
@@ -1,16 +1,9 @@
 import importlib
 import pytest
 from asserts.asserts import assert_true
-from asserts.test_case import TestCase
-
+from asserts.testcase import TestCase
 
 solution = importlib.import_module('katas.6kyu.Array diff.solution').array_diff
-
-
-class TestSolution:
-    def test_array_diff(self):
-        assert_true(array_diff([1, 2], [1]), [2])
-        assert_true(array_diff([1, 2, 2], []), [1, 2, 2])
 
 cases = [
     TestCase([[1, 2], [1]], [2]),
