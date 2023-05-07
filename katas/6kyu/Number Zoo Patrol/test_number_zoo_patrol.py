011df2e66751cb8c4beaa85dc27b31ebc9c2b279
vmred <16134967+vmred@users.noreply.github.com>
2023-05-07 16:27:43 +0300

    fix PR comments
    Co-authored-by: red-redalert <132378585+red-redalert@users.noreply.github.com>

--- katas/6kyu/Number Zoo Patrol/test_number_zoo_patrol.py
+++ katas/6kyu/Number Zoo Patrol/test_number_zoo_patrol.py
@@ -11,5 +11,5 @@ cases = [Case([2, 3, 4], 1), Case([1, 2, 3], 4), Case([1, 2, 4], 3)]
 
 class TestSolution:
     @pytest.mark.parametrize('test', cases, ids=[f'{test.test_data}' for test in cases])
-    def test_find_missing_number(self, test):
+    def test_number_zoo_patrol(self, test):
         assert_true(solution(test.test_data), test.test_output)
--- katas/6kyu/Reverse or rotate/test_reverse_or_rotate.py
+++ katas/6kyu/Reverse or rotate/test_reverse_or_rotate.py
@@ -7,11 +7,13 @@ from asserts.testcase import Case
 solution = importlib.import_module('katas.6kyu.Reverse or rotate.solution').revrot
 
 cases = [
-    Case(["3304991787281570455176064327690480265895", 8], '7199403307518278606715543276904402658958'),
+    Case(["3304991787281570455176064327690480265895", 8], '1994033775182780067155464327690480265895'),
 ]
 
 
 class TestSolution:
+    @pytest.mark.not_completed
+    @pytest.mark.xfail
     @pytest.mark.parametrize('test', cases, ids=[f'{test.test_data}' for test in cases])
     def test_reverse_or_rotate(self, test):
         assert_true(solution(*test.test_data), test.test_output)
