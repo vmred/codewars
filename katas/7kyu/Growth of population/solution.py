d25ef6cf00d1a5d8d54dfe68a10f1a0ce6988465
vmred <16134967+vmred@users.noreply.github.com>
2023-05-11 22:32:57 +0300

    fix test

--- katas/7kyu/Growth of population/solution.py
+++ katas/7kyu/Growth of population/solution.py
@@ -29,7 +29,7 @@
 def nb_year(p0, percent, aug, p):
     count = 0
     while p0 < p:
-        p0 += p0 * percent / 100 + aug
+        p0 += int(p0 * percent / 100 + aug)
         count += 1
 
     return count
