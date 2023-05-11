c0f7ca991b6eaed6b32642d9caf4a5467c12462a
vmred <16134967+vmred@users.noreply.github.com>
2023-05-11 15:49:44 +0300

    Update test_jaden_casing_string.py

--- katas/7kyu/Jaden casing string/test_jaden_casing_string.py
+++ katas/7kyu/Jaden casing string/test_jaden_casing_string.py
@@ -1,12 +1,12 @@
 import importlib
 from asserts.asserts import assert_true
 
-toJadenCase = importlib.import_module('katas.7kyu.Jaden casing string.solution').toJadenCase
+to_jaden_case = importlib.import_module('katas.7kyu.Jaden casing string.solution').to_jaden_case
 
 
 class TestSolution:
     def test_jaden_casing_string(self):
         assert_true(
-            toJadenCase('How can mirrors be real if our eyes aren\'t real'),
+            to_jaden_case('How can mirrors be real if our eyes aren\'t real'),
             'How Can Mirrors Be Real If Our Eyes Aren\'t Real',
         )
