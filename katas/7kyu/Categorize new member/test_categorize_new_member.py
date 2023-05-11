1c8dda73d3edcfb1defc73a95084ce900378a63f
vmred <16134967+vmred@users.noreply.github.com>
2023-05-11 22:23:22 +0300

    fix naming of functions

--- katas/7kyu/Categorize new member/test_categorize_new_member.py
+++ katas/7kyu/Categorize new member/test_categorize_new_member.py
@@ -1,10 +1,10 @@
 import importlib
 from asserts.asserts import assert_true
 
-openOrSenior = importlib.import_module('katas.7kyu.Categorize new member.solution').openOrSenior
+solution = importlib.import_module('katas.7kyu.Categorize new member.solution').open_or_senior
 
 
 class TestSolution:
     def test_categorize_new_member(self):
-        assert_true(openOrSenior([[45, 12], [55, 21], [19, -2], [104, 20]]), ['Open', 'Senior', 'Open', 'Senior'])
-        assert_true(openOrSenior([[16, 23], [73, 1], [56, 20], [1, -1]]), ['Open', 'Open', 'Senior', 'Open'])
+        assert_true(solution([[45, 12], [55, 21], [19, -2], [104, 20]]), ['Open', 'Senior', 'Open', 'Senior'])
+        assert_true(solution([[16, 23], [73, 1], [56, 20], [1, -1]]), ['Open', 'Open', 'Senior', 'Open'])
--- katas/7kyu/Flatten and sort array/solution.py
+++ katas/7kyu/Flatten and sort array/solution.py
@@ -1,6 +1,2 @@
 def flatten_and_sort(array):
-    r = []
-    for i in array:
-        r += i
-
-    return sorted(r)
+    return sorted(sum(array, []))
